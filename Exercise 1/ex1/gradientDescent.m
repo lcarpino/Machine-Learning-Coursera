1a6c4d35e2df4edfbbd510f51f63bd9203d7b77a
Luke Arpino <luke.c.arpino@gmail.com>
2017-04-22 20:28:28 +0100

    completed first exercise

--- Exercise 1/ex1/gradientDescent.m
+++ Exercise 1/ex1/gradientDescent.m
@@ -17,11 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    step = alpha * (X' * (X * theta - y)/m);
+    theta = theta - step;
 
     % ============================================================
 
